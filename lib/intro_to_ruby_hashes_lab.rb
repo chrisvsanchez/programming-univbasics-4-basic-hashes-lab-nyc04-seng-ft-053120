70422c8595633108330083e5dad35843fb410cc4
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-04 00:44:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   new_hash ={:name => "Grace Hopper"}
-  new_hash[name]
+  new_hash[:name]
   
 end
 
