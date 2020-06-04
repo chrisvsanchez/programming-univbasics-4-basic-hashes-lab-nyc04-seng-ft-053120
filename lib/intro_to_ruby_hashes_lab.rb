6b1579316983e9424e6af03b1ffd21d7b75fffbd
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-04 00:43:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,8 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  new_hash ={ Pokemon: "Pikachu"}
-  new_hash[Pokemon]
+  new_hash ={ pokemon: "Pikachu"}
+  new_hash[pokemon]
 end
 
 def pioneer
